bb027c00846cc3732575dc8a308cd2339015e38d
dxrxxn <darwinagraviador89@gmail.com>
2025-07-15 10:15:08 +0800

    Added new file DataStore.php

---
+++ src/Core/DataStore.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Agraviador\Gs\Core;
+
+class DataStore
+{
+    private string $filePath;
+
+    public function __construct(string $filePath)
+    {
+        $this->filePath = $filePath;
+        if (!file_exists($this->filePath)) {
+            file_put_contents($this->filePath, json_encode([]));
+        }
+        // clear data
+        file_put_contents($this->filePath, json_encode([]));
+    }
+    //create a connection to the data store
+    public function getConnection(): array
+    {
+        $data = file_get_contents($this->filePath);
+        return json_decode($data, true) ?? [];
+    }
+}
+
